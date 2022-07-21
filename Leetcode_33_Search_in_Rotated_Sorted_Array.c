000aaf3aa983a9ea32a161d7c45d00f004756a15
steven <steven21470@gmail.com>
2022-07-21 09:46:36 +0800

    Binary search | Levl:M

--- Leetcode_33_Search_in_Rotated_Sorted_Array.c
+++ Leetcode_33_Search_in_Rotated_Sorted_Array.c
@@ -7,7 +7,7 @@ void shiftint(int*,int,int,int);
 void reverse(int*,int,int);
 int search(int*,int,int);
 int cmp(const void* a,const void* b){
-	return *(int *)a - *(int *)b;  // ¤p±Æ¨ì¤j 
+	return *(int *)a - *(int *)b;  // ï¿½pï¿½Æ¨ï¿½j
 }
 
 typedef  struct number{
@@ -16,20 +16,20 @@ typedef  struct number{
 }number;
 
 int main(){
-	number list;	
+	number list;
 	list.number = (int*)malloc(10*sizeof(int));
 	list.size=10;
 	unsigned int seed;
 	seed = time(NULL);
-	srand(seed); // ¥H®É¶¡§Ç¦C·í¶Ã¼ÆºØ¤l
+	srand(seed); // ï¿½Hï¿½É¶ï¿½ï¿½Ç¦Cï¿½ï¿½ï¿½Ã¼ÆºØ¤l
 	printf("number_size : %d \n",list.size);
-	//ÀH¾÷²£¥Í¾ã¼Æ¼Æ¦C 
+	//ï¿½Hï¿½ï¿½ï¿½ï¿½ï¿½Í¾ï¿½Æ¼Æ¦C
 	printf("number[] : ");
 	for(int i=0; i<list.size; ++i){
 		list.number[i]=rand()%10;
 		printf("%d ",list.number[i]);
 	}
-	
+
 	//	void qsort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*))
 	qsort(list.number,list.size,sizeof(int) ,cmp);
 	printf("\nAfter Sort : ");
@@ -41,9 +41,9 @@ int main(){
 	//  ^
 	//    ^
 	// [0 1 2 3 4 5 8 10 x x]
-	
+
 	int size=removeDuplicates(list.number,list.size);
-	
+
 	list.number=(int*)realloc(list.number,sizeof(int)*(size+1));
 	list.size=size;
 	printf("\nnew size:%d",size);
@@ -55,7 +55,7 @@ int main(){
 	int shift=rand()% size;
 	int direction =rand()% 2;
 	printf("\nshift : %d",shift);
-	// 0 left ¡B 1 right
+	// 0 left ï¿½B 1 right
 	printf("\ndirection : %d | 0:left 1:right",direction);
 	shiftint(list.number,list.size,shift,direction);
 	printf("\nAfter shift: ");
@@ -64,7 +64,7 @@ int main(){
 	}
 	int target=5;
 	printf("\nPosition : %d",search(list.number,list.size,target));
-	
+
 }
 
 void shiftint(int*number,int size,int shift,int direction){
@@ -73,7 +73,7 @@ void shiftint(int*number,int size,int shift,int direction){
 	reverse(number,0,shift);
 	reverse(number,shift,size);
 	reverse(number,0,size);
-} 
+}
 void reverse(int*number,int start,int end){
 	end--;
 	while(start<end){
@@ -85,19 +85,19 @@ void reverse(int*number,int start,int end){
 	}
 }
 int removeDuplicates(int* nums, int numsSize){
-	int last=0;	
+	int last=0;
 	for(int i=1;i<numsSize;i++){
 		if(nums[i]!=nums[i-1]){
 			last++;
 			nums[last]=nums[i];
-		}	
+		}
 	}
 	printf("\nDelete Duplicate : ");
 	for(int i=0; i<last+1; ++i){
 		printf("%d ",nums[i]);
 	}
 	return last+1;
-} 
+}
 int search(int* nums, int numsSize, int target){
 	int left=0;
 	int right=numsSize-1;
@@ -120,7 +120,7 @@ int search(int* nums, int numsSize, int target){
 				}
 			}
 		}
-		else if(nums[mid]>target){	
+		else if(nums[mid]>target){
 			if(nums[mid]>=nums[left]){
 				if(target>=nums[left]){
 					right=mid;
@@ -134,6 +134,24 @@ int search(int* nums, int numsSize, int target){
 			}
 		}
 	}
-	
+
 	return -1;
+}
+int search(int* nums, int numsSize, int target){
+    int left=0;
+    int right=numsSize-1;
+    int mid;
+    while(left<=right){
+        mid=(right-left)/2+left;
+        if(nums[mid]==target) return mid;
+        if(nums[mid]<nums[right]){
+            if(nums[mid]<target && nums[right]>=target)  left=mid+1;
+            else right=mid-1;
+        }
+        else{
+            if(nums[left]<=target && nums[mid]>target) right=mid-1;
+            else left=mid+1;
+        }
+    }
+    return -1;
 }
