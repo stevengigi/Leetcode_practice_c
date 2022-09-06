8662ab75edfc101acbcfc083f3634bb6b456c7f8
steven <steven21470@gmail.com>
2022-09-06 10:39:56 +0800

    DFS | Level:M

---
+++ Leetcode_814_Binary_Tree_Pruning.c
@@ -0,0 +1,10 @@
+struct TreeNode* pruneTree(struct TreeNode* root){
+    if(!root) return NULL;
+    root->left=pruneTree(root->left);
+    root->right=pruneTree(root->right);
+    if(root->left==NULL && root->right==NULL && root->val==0){
+        return NULL;
+    }
+
+    return root;
+}
