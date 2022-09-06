71464881f101a9ce5e670eb904b7382029415475
steven <steven21470@gmail.com>
2022-09-07 01:10:35 +0800

    Floyd's Cycle Detection | Level:M

--- Leetcode_287_Find_the_Duplicate_Number.c
+++ Leetcode_287_Find_the_Duplicate_Number.c
@@ -1,9 +1,22 @@
 
 int findDuplicate(int* nums, int numsSize){
-    int *t=(int*)calloc(numsSize,sizeof(int));
-    for(int i=0;i<numsSize;i++){
-        if(t[nums[i]]++) return nums[i];
-        t[nums[i]]++;
+    int slow=0;
+    int fast=0;
+    while(true){
+        slow=nums[slow];
+        fast=nums[nums[fast]];
+        if(slow==fast){
+            break;
+        }
+    }
+    printf("%d",fast);
+    slow=0;
+    while(true){
+        slow=nums[slow];
+        fast=nums[fast];
+        if(slow==fast){
+            return slow;
+        }
     }
     return;
 }
