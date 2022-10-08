a5788c19f746076492148819e91879ae988b7c21
steven <steven21470@gmail.com>
2022-10-08 13:39:44 +0800

    Level:E

--- Leetcode_136_Single_Number.c
+++ Leetcode_136_Single_Number.c
@@ -1,9 +1,14 @@
-
-
+#include<stdio.h>
+#include<stdlib.h>
 int singleNumber(int* nums, int numsSize){
     int ans=0;
     for(int i=0;i<numsSize;i++){
         ans ^=nums[i];
     }
     return ans;
+}
+
+int main(){
+    int nums[5]={2,2,1,1,5};
+    printf("%d",singleNumber(nums,sizeof(nums)/sizeof(int)));
 }
