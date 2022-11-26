f5acc4b86ff08c49825e4430434d9560acc90b9f
steven <steven21470@gmail.com>
2022-11-26 22:44:25 +0800

    Level:E

---
+++ Leetcode_617_Merge_Two_Binary_Trees.c
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* mergeTrees(struct TreeNode* root1, struct TreeNode* root2){
+    if(!root1) return root2;
+    if(!root2) return root1;
+    root1->val+=root2->val;
+    root1->right=mergeTrees(root1->right,root2->right);
+    root1->left=mergeTrees(root1->left,root2->left);
+     return root1;
+}
