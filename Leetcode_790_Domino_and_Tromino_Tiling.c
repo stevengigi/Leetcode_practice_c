2f24355ddd8426ae9e2fe34d5e53dec3d57c112f
steven <steven21470@gmail.com>
2022-12-25 00:51:29 +0800

    DFS | Level:M

---
+++ Leetcode_790_Domino_and_Tromino_Tiling.c
@@ -0,0 +1,25 @@
+#define e 1000000007
+int numTilings(int n){
+    long *dp=(long*)malloc(sizeof(long)*1001);
+    dp[0]=1;
+    dp[1]=1; // |
+    dp[2]=2;//  ||  =
+    dp[3]=5; // |||  =|  |=  「」  ﹄﹃ ={dp[2]+{|}  ,  dp[1]+{=}  , dp[0]+{「」  ﹄﹃}}
+    // dp[4] =11;
+    //dp[4] = dp[3]+{|} , dp[2]+{=} , dp[1] + {﹄﹃  「」} , dp[0]+{『-﹃ 、﹄-』}
+    //dp[n]=dp[n-1]+dp[n-2]+2*(dp[n-3]+...dp[0]);
+    // dp[n-1]=dp[n-2]+dp[n-3]+2*(dp[n-4]+..dp[0]);
+    // dp[n]-dp[n-1]=dp[n-1]+dp[n-3];
+    // dp[n]=2*dp[n-1]+dp[n-3];
+    int temp;
+    if(n<=3){
+        temp=dp[n];
+    }else{
+        for(int i=4;i<=n;i++){
+            dp[i]=(2*dp[i-1]+dp[i-3])%e;
+        }
+        temp=dp[n];
+    }
+    free(dp);
+    return temp;
+}
