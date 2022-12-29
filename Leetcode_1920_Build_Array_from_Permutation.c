88b87afa920f04f15f8796f5e9fb083f748cf0a2
steven <steven21470@gmail.com>
2022-12-29 19:48:31 +0800

    Level:E

---
+++ Leetcode_1920_Build_Array_from_Permutation.c
@@ -0,0 +1,12 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* buildArray(int* nums, int numsSize, int* returnSize){
+    int *arr=(int*)malloc(sizeof(int)*numsSize);
+    // int *ans=(int*)malloc(sizeof(int)*numsSize);
+    for(int i=0;i<numsSize;i++){
+       arr[i]=nums[nums[i]];
+    }
+    *returnSize=numsSize;
+    return arr;
+}
